c3c2e9cf389ef3e3dd297700725866644152cfda
ckstnr99 <ckstnr99@naver.com>
2025-03-03 15:08:18 +0900

    2025.03.03. 15:08 project created

---
+++ community_feed/src/main/kotlin/com/kotlin_example/community_feed/CommunityFeedApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin_example.community_feed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunityFeedApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunityFeedApplication>(*args)
+}
---
+++ community_feed/src/test/kotlin/com/kotlin_example/community_feed/CommunityFeedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin_example.community_feed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunityFeedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
