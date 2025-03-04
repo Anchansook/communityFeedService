57427eee0d588725d95d3c23304fa1ae00c4a964
ckstnr99 <ckstnr99@naver.com>
2025-03-04 16:50:50 +0900

    2025.03.04. 16:50 Post, PostContent

---
+++ community_feed/src/main/java/com/fastcampus/community_feed/post/domain/Post.java
@@ -0,0 +1,22 @@
+package com.fastcampus.community_feed.post.domain;
+
+import com.fastcampus.community_feed.user.domain.User;
+
+//% 게시글 
+
+public class Post {
+
+	private final Long id;
+	private final User author;
+	private final PostContent content;
+
+	public Post(Long id, User author, PostContent content) {
+		// 작성자가 없다면 에러 
+		if (author == null) throw new IllegalArgumentException();
+
+		this.id = id;
+		this.author = author;
+		this.content = content;
+	}
+	
+}
