827e6f7bbe249a9515a333caf5af1aaa22c950fd
ckstnr99 <ckstnr99@naver.com>
2025-03-04 11:33:25 +0900

    2025.03.04. 11:33 UserRelationCounter

--- community_feed/src/main/java/com/fastcampus/community_feed/user/domain/User.java
+++ community_feed/src/main/java/com/fastcampus/community_feed/user/domain/User.java
@@ -5,10 +5,20 @@ import java.util.Objects;
 public class User {
 	private final Long id;
 	private final UserInfo userInfo;
+	private final UserRelationCounter followingCounter;
+	private final UserRelationCounter followerCounter;
 
 	public User(Long id, UserInfo userInfo) {
 		this.id = id;
 		this.userInfo = userInfo;
+		// 각각으로 사용을 해야하기 때문에 새 객체를 생성하여 설정
+		this.followingCounter = new UserRelationCounter();
+		this.followerCounter = new UserRelationCounter();
+	}
+
+	// 팔로우
+	public void follow(User targetUser) {
+		if (targetUser.equals(this)) throw new IllegalArgumentException();
 	}
 
 	@Override
@@ -25,5 +35,4 @@ public class User {
 		return Objects.hashCode(id);
 	}
 
-	// 잠시 확인용 다음 공부 때 지워~
 }
