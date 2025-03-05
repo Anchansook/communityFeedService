2ac320235b1178fa6b3d338c86c7c7938a476d54
ckstnr99 <ckstnr99@naver.com>
2025-03-05 12:57:39 +0900

    2025.03.05. 12:57 PostPublicationState, Post

--- community_feed/src/main/java/com/fastcampus/community_feed/post/domain/Post.java
+++ community_feed/src/main/java/com/fastcampus/community_feed/post/domain/Post.java
@@ -2,6 +2,7 @@ package com.fastcampus.community_feed.post.domain;
 
 import com.fastcampus.community_feed.common.domain.PositivelntegerCounter;
 import com.fastcampus.community_feed.post.domain.content.PostContent;
+import com.fastcampus.community_feed.post.domain.content.PostPublicationState;
 import com.fastcampus.community_feed.user.domain.User;
 
 //% 게시글 
@@ -12,6 +13,7 @@ public class Post {
 	private final User author;
 	private final PostContent content;
 	private final PositivelntegerCounter likeCount;
+	private PostPublicationState state; // 상태 변경이 되어야 하므로 final 선언 안 함
 
 	// 생성자
 	public Post(Long id, User author, PostContent content) {
@@ -22,6 +24,7 @@ public class Post {
 		this.author = author;
 		this.content = content;
 		this.likeCount = new PositivelntegerCounter(); // 매개변수로 받아오는 것이 아니라 생성 시 0으로 초기화로 시작
+		this.state = PostPublicationState.PUBLIC; // 처음엔 전체 공개
 	}
 
 	// 좋아요 기능
@@ -39,8 +42,10 @@ public class Post {
 	}
 
 	// 게시물 수정
-	public void updatePost(User user, String updateContent) {
+	public void updatePost(User user, String updateContent, PostPublicationState state) {
 		if (!this.author.equals(user)) throw new IllegalArgumentException();
+
+		this.state = state; // 수정 시 상태 변경, 변경된 상태로 설정
 		this.content.updateContent(updateContent);
 	}
 	
---
+++ community_feed/src/main/java/com/fastcampus/community_feed/post/domain/content/PostPublicationState.java
@@ -0,0 +1,11 @@
+package com.fastcampus.community_feed.post.domain.content;
+
+//% 게시물 상태 enum
+
+public enum PostPublicationState {
+
+	PUBLIC, // 전체 공개
+	ONLY_FOLLOWER, // 팔로워들만 공개
+	PRIVATE // 나만 공개
+	
+}
